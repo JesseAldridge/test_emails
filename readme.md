140109b4afaa6d7f1863907facac6c2bf9d73e90
Jesse Aldridge <jesse.aldridge@airbnb.com>
2017-05-05 15:58:33 -0700

    first

---
+++ readme.md
@@ -0,0 +1,7 @@
+Create `config.py` file
+
+Install requirements:  `pip install --user requirements.txt`
+
+Configure remote environment
+
+Run the test:  `python test_emails.py`
---
+++ test_emails.py
@@ -0,0 +1,49 @@
+import os, subprocess, json, glob, shutil
+
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+import config
+
+# It's an invalid ssl cert, but we're just testing locally so it's fine
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+headers = {'content-type': 'application/json'}
+
+responses_dir = os.path.join('timekit_data', 'responses')
+if not os.path.exists(responses_dir):
+  os.makedirs(responses_dir)
+
+for test_dict in [
+  {'endpoint': 'created', 'filename': 'confirmed.json'},
+  {'endpoint': 'canceled_by_host', 'filename': 'confirmed.json'},
+  {'endpoint': 'canceled_by_partner', 'filename': 'confirmed.json'},
+  {'endpoint': 'send_reminders', 'filename': 'confirmed.json'},
+  {'endpoint': 'send_reminders', 'filename': 'confirmed_no_phone.json'},
+]:
+  filename = test_dict.get('filename')
+  endpoint = test_dict.get('endpoint')
+
+  with open(os.path.join('timekit_data', filename)) as f:
+    json_text = f.read()
+  data = json.loads(json_text)
+
+  url = '{}/collections_application/booking/{}'.format(config.base_url, endpoint)
+  print 'posting to: {} with test file: {}'.format(url, filename)
+  resp = requests.post(url, json=data, headers=headers, verify=False)
+  with open(os.path.join(responses_dir, endpoint) + '.html', 'w') as f:
+    f.write(resp.content)
+  assert resp.status_code == 200
+
+print 'rsyncing emails...'
+
+local_emails_path = os.path.expanduser(config.local_emails_path)
+
+command_line = 'rsync -t {}@{}:{}* {}'.format(
+  config.user_name, config.host_name, config.remote_emails_path, local_emails_path)
+command_list = command_line.split()
+subprocess.call(command_list)
+
+for old_path in glob.glob(os.path.join(local_emails_path, '*.eml')):
+  new_path = old_path.rsplit('.', 1)[0] + '.html'
+  shutil.move(old_path, new_path)
